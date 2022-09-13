551dc4f3d9168d8a99bd579e47ba117b30f49894
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-13 10:29:32 +0200

    Create maven project

---
+++ src/test/java/de/allianz/FEFEFEFEF/FefefefefApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.FEFEFEFEF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FefefefefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
